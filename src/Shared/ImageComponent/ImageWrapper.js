ff90068a1b7a886d3fc70abab246361ebf8c5e29
DAVID HO <davidhorph@hotmail.com>
2023-01-23 23:46:14 -0600

    Added img width 150px min-height 100% to make them even.

--- src/Shared/ImageComponent/ImageWrapper.js
+++ src/Shared/ImageComponent/ImageWrapper.js
@@ -3,7 +3,7 @@ import './ImageWrapper.styles.css';
 
 const ImageWrapper = ({ image, description }) => {
 
-  return <img src={image}  alt={description}/>
+  return <img src={image}  alt={description} className='img-thumbnail'/>
 }
 
 export default ImageWrapper
